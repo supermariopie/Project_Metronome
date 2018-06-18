78cc01d586cfef8eb4c594faae8d17b6853a0803
supermariopie <supermariopie8@gmail.com>
2018-06-18 16:17:35 -0400

    Something
    hi

--- Metronome.py
+++ Metronome.py
@@ -1,1 +1,30 @@
+import threading
+import sys
+import simpleaudio
 
+command = ""
+bm = 100
+sound_name = ""
+
+def play_sound(bm, sound_name):
+	#plays the sound here!
+	
+	print("playing a sound!")
+
+	
+def stop_sound():
+	
+	pass
+	
+	
+while (command != "q"):
+	bm = input("input speed");
+	sound_name = input("input sound name");
+	
+	signal = threading.Event();
+	signal.clear();
+	
+	sound_thread = threading.Thread(target=play_sound,args=(bm,sound_name))
+	stop_thread = threading.Thread(target=stop_sound)
+	
+	
