a0c32e6e1769d39318b899a3fce9a61c986b2447
jferragut666 <jacobferragut@gmail.com>
2018-06-21 16:34:56 -0400

    The finished metronome yay

--- AndrewTestMultiP.py
+++ AndrewTestMultiP.py
@@ -1,39 +1,51 @@
-import multiprocessing
-import time
-import simpleaudio
-    
+import multiprocessing, time
+
+from pygame import mixer
+mixer.pre_init(44100, -16, 1, 512)
+mixer.init()
+sound = mixer.Sound('bass.wav')
 
 #this function plays a sound until the computer breaks down or the thread is terminated
 def play_sound(bm):
     while(True):
-        print ("bye")
-        time.sleep(.5)
+        sound.play()
+        time.sleep(60/bm)
 
 #idk why this is here
 if __name__ == "__main__":
 
     #the quit variable is used so the client can use the metronome multiple times
-    quit="continue"
+    hola="continue"
     bm = 100
     
-    while (quit!="quit"):
+    while (hola!="quit"):
         
         print("Welcome to the met!")
-        hola="go"
-        
-        bm=int(input("Please input the speed of the metronome"))
-        
+    
+        try:
+            bm=int(input("Please input the speed of the metronome: "))
+        except:
+            print("Please type in an int next time")
+            bm=100
+            
         m2 = multiprocessing.Process(target=play_sound,args=(bm,))
 
         m2.start()
-
+        
         while (hola!="stop"):
-            hola = input("input?")
-            
+            hola = input("Type stop to stop, or an int to set a new speed: ")
+            try:
+                hola = int(hola)
+                m2.terminate()
+                m2 = multiprocessing.Process(target=play_sound,args=(int(hola),))
+                m2.start()
+            except:
+                pass
+               
             
         
         m2.terminate()
-        quit=input("Do you want to continue or quit")
+        hola=input("Do you want to continue or quit, type quit if you want to quit: ")
     
     print()
     print ("Thank you for using our metronome!")
